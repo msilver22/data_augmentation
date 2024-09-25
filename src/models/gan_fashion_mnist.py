6871e6aa521ad915f37dbc0275953647fb67b3fe
SIL._.VER22 <151758027+msilver22@users.noreply.github.com>
2024-09-25 17:18:21 +0200

    Update gan_fashion_mnist.py

--- src/models/gan_fashion_mnist.py
+++ src/models/gan_fashion_mnist.py
@@ -4,10 +4,8 @@ import lightning as L
 import numpy as np
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(f'Using device: {device}')
 BATCH_SIZE = 64
 NUM_WORKERS = int(os.cpu_count() / 2)
-print(f'Number of workers: {NUM_WORKERS}')
 PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
 
 class Generator(nn.Module):
